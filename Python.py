b8bc9a2aa6bbc15761582aa6ef0e132a64a2d789
0-122-8 <0-122-8>
2021-02-12 19:14:58 +0500

    First + Second + Third exersices

--- Python.py
+++ Python.py
@@ -22,5 +22,21 @@ def promotion():
     return '</br>'.join(promotion_list)
 
 
+@app.route('/image_mars')
+def image_mars():
+    return """<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <title>Привет, Марс!</title>
+                      </head>
+                      <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="/static/img/mars.jpg" alt="здесь должна была быть картинка, но не нашлась">
+                        <p>Вот она какая, красная планета.</p>
+                      </body>
+                    </html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
