dd1cfdf894d5ab068cacf946448866c4df1a3e08
0-122-8 <0-122-8>
2021-02-16 19:57:28 +0500

    Classwork exersices

--- Python.py
+++ Python.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, url_for
+import os
 
 
 app = Flask(__name__)
@@ -6,7 +7,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def start_page():
-    return 'Миссия Колонизация Марса'
+    return """<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <title>Привет, Марс!</title>
+                      </head>
+                      <body>
+                        <p>Миссия колонизация Марса</p>
+                        </br>
+                        <p><a href="/">Стартовая страница</a></p>
+                        <p><a href="index">Колонизация Марса</a></p>
+                        <p><a href="promotion">Рекламная кампания</a></p>
+                        <p><a href="image_mars">Изображение Марса</a></p>
+                        <p><a href="promotion_image">Реклама с картинкой</a></p>
+                        <p><a href="selection">Отбор астронавтов</a></p>
+                        </br>
+                      </body>
+                    </html>"""
 
 
 @app.route('/index')
@@ -177,6 +195,77 @@ def promotion_image():
                     </div>
                   </body>
                 </html>'''
+
+
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def greeting(username):
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                   <link rel="stylesheet"
+                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                   crossorigin="anonymous">
+                    <title>Привет, {username}</title>
+                  </head>
+                  <body>
+                    <h1>Привет, {username}!</h1>
+                  </body>
+                </html>'''
+
+
+@app.route('/load_photo', methods=['POST', 'GET'])
+def sample_file_upload():
+    if request.method == 'GET':
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/form.css')}" />
+                            <title>...</title>
+                          </head>
+                          <body>
+                            <h1>Загрузим файл</h1>
+                            <div>
+                            <form method="post" enctype="multipart/form-data">
+                               <div class="form-group">
+                                    <label for="photo">Выберите файл</label>
+                                    <input type="file" class="form-control-file" id="photo" name="file">
+                                </div>
+                                <button type="submit" class="btn btn-primary">Отправить</button>
+                            </form>
+                            </div>
+                          </body>
+                        </html>'''
+    elif request.method == 'POST':
+        f = request.files['file']
+        with open('image.jpeg', "wb") as file:
+            file.write(f.read())
+        os.replace('image.jpeg', 'static/img/image.jpeg')
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/form.css')}" />
+                            <title>Пример загрузки файла</title>
+                          </head>
+                          <body>
+                            <h1>Загрузим файл</h1>
+                            <form method="post" enctype="multipart/form-data">
+                               <div class="form-group">
+                                    <label for="photo">Выберите файл</label>
+                                    <input type="file" class="form-control-file" id="photo" name="file">
+                                </div>
+                                <img src="/static/img/image.jpeg" alt="здесь должна была быть картинка, но не нашлась">
+                                </br>
+                                <button type="submit" class="btn btn-primary">Отправить</button>
+                            </form>
+                          </body>
+                        </html>'''
+
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
